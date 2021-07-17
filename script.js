996e9f449cc01b47ee4db3e928f5989f1991892e
Wilker Lisboa <wilkerlisboa.ofc@gmail.com>
2021-07-17 18:13:42 -0300

    First commit

---
+++ script.js
@@ -0,0 +1,7 @@
+function adm(){
+    var nome = document.getElementById('name_of_input').value;
+    var email = document.getElementById("email_of_input").value;
+
+    var result = document.getElementById("resultado").innerHTML = nome;
+}
+
