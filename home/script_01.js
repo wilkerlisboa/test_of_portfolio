8bf475b5c5550d56a4e14c3cecfa5d5a6cd3f055
Wilker Lisboa <wilkerlisboa.ofc@gmail.com>
2021-09-12 15:45:05 -0300

    Seventh commit

--- home/script_01.js
+++ home/script_01.js
@@ -15,12 +15,23 @@ function writeText(){
 setInterval(writeText, 200);
 
 //RELOAD ANIMATION CLICK ON BUTTON
-var botaoAserAnimado = document.querySelector(".menu");
+var botaoAserAnimado = document.querySelector(".Home_menu");
 
 botaoAserAnimado.onclick = function efeito() {
    var e = document.getElementById("home");
    e.style.animation = "none";
    setTimeout(function() {
       e.style.animation = "";
+   }, 100);
+}
+
+//RELOAD ANIMATION CLICK ON BUTTON
+var TwobotaoAserAnimado = document.querySelector(".About_menu");
+
+TwobotaoAserAnimado.onclick = function efeito() {
+   var b = document.getElementById("about");
+   b.style.animation = "none";
+   setTimeout(function() {
+      b.style.animation = "";
    }, 100);
 }
